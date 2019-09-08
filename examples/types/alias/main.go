dad36955a362e585c1615c850427ae8dd9e90b4d
yiqing <yiqing_95@qq.com>
2019-09-08 23:44:14 +0800

    测试类型别名特性

--- examples/types/alias/main.go
+++ examples/types/alias/main.go
@@ -5,9 +5,14 @@ import (
 )
 
 // ## 类型别名和原类型完全一样，只不过是另一种叫法而已
+// 完全一样(identical types)意味着这两种类型的数据可以互相赋值，而类型定义要和原始类型赋值的时候需要类型转换(ConversionT(x))。
+
+// 进一步想，这样是不是我们可以为其它库中的类型增加新的方法了， 比如为标准库的time.Time增加一个滴答方法
+// 答案是:NO , 编译的时候会报错:cannot define new methods on non-local type time.Time。
 
 type AddSum = int //给 int 取一个别名叫 AddSum
 
+// NOTE  这里就是 不能为其他包中的类型定义方法！
 //func (this AddSum)printMyType() {
 //	fmt.Printf("AddSum underline type is %T \n", this)
 //}
@@ -20,6 +25,7 @@ func (this NewInt) printMyType() {
 func main() {
 	var a AddSum
 	fmt.Printf("%T\n", a)
+
 	var a2 NewInt
 	fmt.Printf("%T\n", a2)
 
