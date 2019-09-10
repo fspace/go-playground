1aacf46dc3cdedb164f9c1541246ca650e0d08a4
yiqing <yiqing_95@qq.com>
2019-09-10 21:02:04 +0800

    测试excel文件写入

---
+++ awesomelib/xlsx/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"github.com/tealeg/xlsx"
+)
+
+func main() {
+	var file *xlsx.File
+	var sheet *xlsx.Sheet
+	var row *xlsx.Row
+	var cell *xlsx.Cell
+	var err error
+
+	file = xlsx.NewFile()
+	sheet, err = file.AddSheet("Sheet1")
+	if err != nil {
+		fmt.Printf(err.Error())
+	}
+	row = sheet.AddRow()
+	cell = row.AddCell()
+	cell.Value = "I am a cell!"
+	err = file.Save("MyXLSXFile.xlsx")
+	if err != nil {
+		fmt.Printf(err.Error())
+	}
+}
