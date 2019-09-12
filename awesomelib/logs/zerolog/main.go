a2293a55001193abcc2c4d0d2479122ee34fb228
yiqing <yiqing_95@qq.com>
2019-09-13 03:03:40 +0800

    测试日志库 zerolog

---
+++ awesomelib/logs/zerolog/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func main() {
+	// UNIX Time is faster and smaller than most timestamps
+	// If you set zerolog.TimeFieldFormat to an empty string,
+	// logs will write with UNIX time
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+
+	log.Print("hello world")
+
+	log.Debug().
+		Str("Scale", "833 cents").
+		Float64("Interval", 833.09).
+		Msg("Fibonacci is everywhere")
+
+	log.Debug().
+		Str("Name", "Tom").
+		Send()
+}
