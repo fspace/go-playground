a941f65602f60a9a28f063e7e4f89d6b85d51502
yiqing <yiqing_95@qq.com>
2019-11-10 13:40:35 +0800

    web知识 源于无闻的系列博客

--- webexamples/web-basic/main.go
+++ webexamples/web-basic/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"github.com/fatih/structs"
-	"github.com/imdario/mergo" // 相当好用的库 被多个著名项目使用哦
+	"github.com/imdario/mergo" // 相当好用的库 被多个著名项目使用哦  用法如： https://github.com/godep-migrator/rigger-host/blob/master/host/config.go
+	"strconv"
+
 	//"github.com/mitchellh/mapstructure"
 	"log"
 	"net/http"
@@ -22,6 +24,18 @@ Quantity: {{.Quantity}}
 
 SiteName : {{ .SiteName }}
 CurrentUser: {{.User}}
+`
+		// 模板也可以逐步构造："golang 几种字符串的连接方式" https://segmentfault.com/a/1190000012978989
+		tplStr = `
+{{$name := "Alice"}}
+{{$age := 18}}
+{{$round2 := true}}
+Name: {{$name}}
+Age: {{$age}}
+Round2: {{$round2}}
+
+{{$name = "Bob"}}
+Name: {{$name}}
 `
 		tmpl, err := template.New("test").Parse(tplStr)
 		if err != nil {
@@ -49,7 +63,7 @@ CurrentUser: {{.User}}
 			"Quantity":  5,
 		}
 		// 跟基础数据做合并
-		mergo.Merge(&baseData, currentData)
+		mergo.Merge(&baseData, currentData) // 和jquery  extend  有点像呢！
 
 		fmt.Printf("%#v", baseData)
 		err = tmpl.Execute(w, baseData)
@@ -59,12 +73,210 @@ CurrentUser: {{.User}}
 		}
 	})
 
+	// localhost:4000/div?x=1&y=2
+	http.HandleFunc("/div", divHandler)
+	http.HandleFunc("/compare", compareHandler)
+	http.HandleFunc("/range", rangeDemo)
+	http.HandleFunc("/map", mapDemo)
+	http.HandleFunc("/with", withDemo)
+
 	log.Println("Starting HTTP server...")
 	log.Fatal(http.ListenAndServe("localhost:4000", nil))
 }
+
+// ------------------------------------------------------------------------------------ handlers
+func divHandler(w http.ResponseWriter, r *http.Request) {
+	// 创建模板对象并解析模板内容
+	tmpl, err := template.New("test").Parse(`
+{{if .yIsZero}}
+	除数不能为 0
+{{else}}
+	{{.result}}
+{{end}}
+`)
+	if err != nil {
+		fmt.Fprintf(w, "Parse: %v", err)
+		return
+	}
+
+	// 获取 URL 查询参数的值
+	// 注意：为了简化代码逻辑，这里并没有进行错误处理
+	x, _ := strconv.ParseInt(r.URL.Query().Get("x"), 10, 64)
+	y, _ := strconv.ParseInt(r.URL.Query().Get("y"), 10, 64)
+
+	// 当 y 不为 0 时进行除法运算
+	yIsZero := y == 0
+	result := 0.0
+	if !yIsZero {
+		result = float64(x) / float64(y)
+	}
+
+	// 调用模板对象的渲染方法
+	err = tmpl.Execute(w, map[string]interface{}{
+		"yIsZero": yIsZero,
+		"result":  result,
+	})
+	if err != nil {
+		fmt.Fprintf(w, "Execute: %v", err)
+		return
+	}
+}
+
+func compareHandler(w http.ResponseWriter, r *http.Request) {
+	// 创建模板对象并解析模板内容
+	tmpl, err := template.New("test").Parse(`
+{{$name1 := "alice"}}
+{{$name2 := "bob"}}
+{{$age1 := 18}}
+{{$age2 := 23}}
+
+{{if eq $age1 $age2}}
+	年龄相同
+{{else}}
+	年龄不相同
+{{end}}
+
+{{if ne $name1 $name2}}
+	名字不相同
+{{end}}
+
+{{if gt $age1 $age2}}
+	alice 年龄比较大
+{{else}}
+	bob 年龄比较大
+{{end}}
+`)
+	if err != nil {
+		fmt.Fprintf(w, "Parse: %v", err)
+		return
+	}
+
+	// 调用模板对象的渲染方法
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		fmt.Fprintf(w, "Execute: %v", err)
+		return
+	}
+}
+
+func rangeDemo(w http.ResponseWriter, r *http.Request) {
+	// 创建模板对象并解析模板内容
+	tmpl, err := template.New("test").Parse(`
+{{range $name := .Names}}
+	{{$name}}
+{{end}}
+
+{{range $i, $name := .Names}}
+	{{$i}}. {{$name}}
+{{end}}
+`)
+	if err != nil {
+		fmt.Fprintf(w, "Parse: %v", err)
+		return
+	}
+
+	// 调用模板对象的渲染方法
+	err = tmpl.Execute(w, map[string]interface{}{
+		"Names": []string{
+			"Alice",
+			"Bob",
+			"Carol",
+			"David",
+		},
+	})
+	if err != nil {
+		fmt.Fprintf(w, "Execute: %v", err)
+		return
+	}
+}
+
+func mapDemo(w http.ResponseWriter, r *http.Request) {
+	// 创建模板对象并解析模板内容
+	tmpl, err := template.New("test").Parse(`
+{{range $name, $val := .}}
+	{{$name}}: {{$val}}
+{{end}}
+`)
+	if err != nil {
+		fmt.Fprintf(w, "Parse: %v", err)
+		return
+	}
+
+	// 调用模板对象的渲染方法
+	err = tmpl.Execute(w, map[string]interface{}{
+		"Names": []string{
+			"Alice",
+			"Bob",
+			"Carol",
+			"David",
+		},
+		"Numbers": []int{1, 3, 5, 7},
+	})
+	if err != nil {
+		fmt.Fprintf(w, "Execute: %v", err)
+		return
+	}
+}
+
+type Inventory struct {
+	SKU       string
+	Name      string
+	UnitPrice float64
+	Quantity  int64
+}
+
+func withDemo(w http.ResponseWriter, r *http.Request) {
+	// 创建模板对象并解析模板内容
+	tmpl, err := template.New("test").Parse(`Inventory
+SKU: {{.Inventory.SKU}}
+Name: {{.Inventory.Name}}
+UnitPrice: {{.Inventory.UnitPrice}}
+Quantity: {{.Inventory.Quantity}}
+
+========================
+
+{{with .Inventory}}
+	SKU: {{.SKU}}
+	Name: {{.Name}}
+	UnitPrice: {{.UnitPrice}}
+	Quantity: {{.Quantity}}
+{{end}}
+
+================================
+blank
+{{- with .Inventory}}
+	SKU: {{.SKU}}
+	Name: {{.Name}}
+	UnitPrice: {{.UnitPrice}}
+	Quantity: {{.Quantity}}
+{{- end}}
+
+`)
+	if err != nil {
+		fmt.Fprintf(w, "Parse: %v", err)
+		return
+	}
+
+	// 调用模板对象的渲染方法
+	err = tmpl.Execute(w, map[string]interface{}{
+		"Inventory": Inventory{
+			SKU:       "11000",
+			Name:      "Phone",
+			UnitPrice: 699.99,
+			Quantity:  666,
+		},
+	})
+	if err != nil {
+		fmt.Fprintf(w, "Execute: %v", err)
+		return
+	}
+}
+
+// ------------------------------------------------------------------------------------ handlers.
 
 // 结构体转Map
 func struct2map(s interface{}) map[string]interface{} {
 	// https://stackoverflow.com/questions/23589564/function-for-converting-a-struct-to-map-in-golang
 	return structs.Map(s)
+	// 这个方法也挺好的： FillMap 可能也可以完成map合并功能
 }
