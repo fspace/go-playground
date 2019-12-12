65c67e180187941937edd1a2e84a29330991bd4c
yiqing <yiqing_95@qq.com>
2019-12-13 00:17:41 +0800

    控制台应用例子

---
+++ consoleapps/progressbar-demo/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"time"
+
+	"github.com/cheggaaa/pb/v3"
+)
+
+func main() {
+	count := 100000
+	// create and start new bar
+	bar := pb.StartNew(count)
+
+	// start bar from 'default' template
+	// bar := pb.Default.Start(count)
+
+	// start bar from 'simple' template
+	// bar := pb.Simple.Start(count)
+
+	// start bar from 'full' template
+	// bar := pb.Full.Start(count)
+
+	for i := 0; i < count; i++ {
+		bar.Increment()
+		time.Sleep(time.Millisecond)
+	}
+	bar.Finish()
+}
