19aa231eb29b00a8baa1d782336ec9d6b030f75a
yiqing <yiqing_95@qq.com>
2019-12-25 14:27:12 +0800

    如何写main方法

---
+++ proexamples/001maindemo/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+// @see https://github.com/hashicorp/consul/blob/master/main.go
+func main() {
+	os.Exit(realMain())
+}
+
+func realMain() (exitCode int) {
+
+	defer func() {
+		// 如果捕获错误 那么返回码就是其他的了
+		if err := recover(); err != nil {
+			fmt.Fprintf(os.Stderr, "Error executing CLI: %s\n", err)
+			exitCode = 1
+		}
+	}()
+
+	return
+}
