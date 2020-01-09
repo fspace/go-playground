7b0db5f904f3d0ffa3b45fc0f18616b0e12d458f
yiqing <yiqing_95@qq.com>
2020-01-09 16:31:56 +0800

    exec 标准库 执行系统命令

---
+++ stdlib/exec/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jawher/mow.cli"
+	"os"
+	"os/exec"
+	"runtime"
+)
+
+func main() {
+	os.Exit(realMain())
+}
+func realMain() (exitCode int) {
+	app := cli.App("fuction-demo", "demo for function type")
+	// --------------------------------------------------------------------------------------- />
+	//			## cmd 配置  如果命令行分散在各个包或者库中 此处会是集成点
+	app.Command("os-info", "CheckingCurrentOperatingSystem ", cli.ActionCommand(CheckingCurrentOperatingSystem))
+
+	// ---------------------------------------------------------------------------------------------
+	// With the app configured, execute it, passing in the os.Args array
+	app.Run(os.Args)
+
+	return
+}
+
+// ==============================================================================
+func basics() {
+
+}
+
+func CheckingCurrentOperatingSystem() {
+	if runtime.GOOS == "windows" {
+		fmt.Println("Can't Execute this on a windows machine")
+	} else {
+		//	execute()
+		fmt.Println("*nix os ")
+	}
+}
+func execute() {
+
+	// here we perform the pwd command.
+	// we can store the output of this in our out variable
+	// and catch any errors in err
+	out, err := exec.Command("ls").Output()
+
+	// if there is an error with our execution
+	// handle it here
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
+	// as the out variable defined above is of type []byte we need to convert
+	// this to a string or else we will see garbage printed out in our console
+	// this is how we convert it to a string
+	fmt.Println("Command Successfully Executed")
+	output := string(out[:])
+	fmt.Println(output)
+
+	// let's try the pwd command herer
+	out, err = exec.Command("pwd").Output()
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
+	fmt.Println("Command Successfully Executed")
+	output = string(out[:])
+	fmt.Println(output)
+}
+
+func execute2() {
+	out, err := exec.Command("ls", "-ltr").Output()
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
+	fmt.Println("Command Successfully Executed")
+	output := string(out[:])
+	fmt.Println(output)
+}
