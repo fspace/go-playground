a346b0985b2e4e61db701077a8fd835a953cb996
yiqing <yiqing_95@qq.com>
2020-01-09 13:37:51 +0800

    控制台程序样版

--- examples/003cli-tpl/main.go
+++ examples/003cli-tpl/main.go
@@ -1,43 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"github.com/jawher/mow.cli"
 	"os"
 )
 
-/**
-- golang 避坑指南(1)interface 之坑多多
-*/
-type Talkable interface {
-	TalkEnglish(string)
-	TalkChinese(string)
-}
-
-type Student1 struct {
-	Talkable // 内嵌接口  按需实现
-	Name     string
-	Age      int
-}
-
-func (s *Student1) TalkEnglish(s1 string) {
-	fmt.Printf("I'm %s,%d years old,%s", s.Name, s.Age, s1)
-}
-
 func main() {
-	defer func() {
-		if r := recover(); r != nil {
-			fmt.Println("panic : ", r)
-		}
-	}()
-	a := Student1{Name: "aaa", Age: 12}
-	a.TalkEnglish("nice to meet you\n")
-
-	a.TalkChinese("汉语呀 你会说么")
-
 	os.Exit(realMain())
 }
-
 func realMain() (exitCode int) {
 	app := cli.App("fuction-demo", "demo for function type")
 	// --------------------------------------------------------------------------------------- />
@@ -52,4 +22,4 @@ func realMain() (exitCode int) {
 	return
 }
 
-// ========================================= =======================================================
+// ==============================================================================
