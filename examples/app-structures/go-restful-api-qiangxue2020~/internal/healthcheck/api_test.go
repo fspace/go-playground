8e3645e189c9858c31b5879a5c8b4ad02b32e73d
yiqing <yiqing_95@qq.com>
2020-02-01 01:57:47 +0800

    强哥删除的项目布局

---
+++ examples/app-structures/go-restful-api-qiangxue2020~/internal/healthcheck/api_test.go
@@ -0,0 +1,17 @@
+package healthcheck
+
+import (
+	"github.com/qiangxue/go-restful-api/internal/test"
+	"github.com/qiangxue/go-restful-api/pkg/log"
+	"net/http"
+	"testing"
+)
+
+func TestAPI(t *testing.T) {
+	logger, _ := log.NewForTest()
+	router := test.MockRouter(logger)
+	RegisterHandlers(router, "0.9.0")
+	test.Endpoint(t, router, test.APITestCase{
+		"ok", "GET", "/healthcheck", "", nil, http.StatusOK, `"OK 0.9.0"`,
+	})
+}
